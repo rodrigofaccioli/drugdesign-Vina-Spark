6d5e8674db800528c85838b613153fb9df8c7a3a
“Rodrigo <rodrigo.faccioli@gmail.com>
2017-08-26 14:38:49 -0300

    Added detect_hbonds

---
+++ src/detect_hbonds/build/CMakeFiles/detect_hbonds.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detect_hbonds.dir/src/detect_hbonds.c.o"
+  "detect_hbonds.pdb"
+  "detect_hbonds"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detect_hbonds.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
